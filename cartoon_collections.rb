c673fe7d752e054f6e87221757e45cd39483ddbf
Sara Amiry <saraamiry24@gmail.com>
2020-06-06 02:26:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,6 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  #planeteer_calls = ["earth", "wind", "fire"]
   planeteer_calls.map! {|word| word.capitalize + '!'}
 end
 
