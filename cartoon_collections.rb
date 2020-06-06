77b83cdd552b35b50623d63914c1c2250b66f556
Sara Amiry <saraamiry24@gmail.com>
2020-06-06 02:31:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.find{|cheese|cheese_types.include?(cheese)}
+  array.find {|cheese| cheese_types.include?(cheese)}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
